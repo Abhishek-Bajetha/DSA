73e5246e67970188681cfb6b3f6fcae02342e90d
Abhishek-Bajetha <158018161+Abhishek-Bajetha@users.noreply.github.com>
2025-03-16 23:28:56 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 class Demo {
     public static void main(String[] args) {
 
-        String x = "Abhishek";
+        String x = "Abhishek bajetha";
         for (int i = 0; i <= 10; i++)
             System.out.println(i + " " + x);
 
