10e279963e53d01212719b49889fdff7a14aec91
Abhishek Bajetha <abhishekbajetha27@gmail.com>
2025-04-26 15:39:01 +0530

    Longest Subarray Sum K Positive Brute force

--- _05_Arrays/Easy/LongestSubarraySumKPositive.java
+++ _05_Arrays/Easy/LongestSubarraySumKPositive.java
@@ -1,28 +1,27 @@
 package _05_Arrays.Easy;
 
 public class LongestSubarraySumKPositive {
-    public static int[] longestSubarraySumKPositive(int arr[], int target) {
-        int sum;
-        int subIndex[] = { -1, -1 };
+    public static int longestSubarraySumKPositive(int arr[], int target) {
+        int sum, largestLength = 0;
         for (int i = 0; i < arr.length; i++) {
             sum = 0;
             for (int j = i; j < arr.length; j++) {
                 sum = sum + arr[j];
                 if (sum == target) {
-                    subIndex[0] = i;
-                    subIndex[1] = j;
-                    return subIndex;
+                    if (largestLength < j - i + 1) {
+                        largestLength = j - i + 1;
+                    }
                 } else if (sum > target) {
                     break;
                 }
             }
         }
-        return subIndex;
+        return largestLength;
     }
 
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-        int subIndex[] = longestSubarraySumKPositive(arr, 7);
-        System.out.println("from index: " + subIndex[0] + " to " + subIndex[1]);
+        int largestLength = longestSubarraySumKPositive(arr, 6);
+        System.out.println("Largest length: " + largestLength);
     }
 }
