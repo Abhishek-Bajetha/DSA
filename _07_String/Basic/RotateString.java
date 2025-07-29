25a003cc24e9d5bca4f0b61a8e20a22a46618250
Abhishek Bajetha <abhishekbajetha27@gmail.com>
2025-07-29 18:30:11 +0530

    Rotate String

---
+++ _07_String/Basic/RotateString.java
@@ -0,0 +1,14 @@
+package _07_String.Basic;
+
+public class RotateString {
+    public static boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length())
+            return false;
+        return (s + s).contains(goal);
+    }
+
+    public static void main(String[] args) {
+        String s = "abcde", goal = "cdeab";
+        System.out.println(rotateString(s, goal));
+    }
+}
