8848ef4b63c6788f72ef418917743e4361d945b7
Abhishek Bajetha <abhishekbajetha27@gmail.com>
2025-09-12 15:14:51 +0530

    Spiral Matrix

---
+++ _05_Arrays/Medium/SpiralMatrix.java
@@ -0,0 +1,47 @@
+package _05_Arrays.Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        int left, right, top, bottom;
+        left = 0;
+        right = matrix[0].length - 1;
+        top = 0;
+        bottom = matrix.length - 1;
+
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                list.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                list.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    list.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    list.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+        List<Integer> answer = spiralOrder(matrix);
+        for (Integer integer : answer) {
+            System.out.print(integer + " ");
+        }
+    }
+}
